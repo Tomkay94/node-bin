9d76b26e89d24eef87edcece453a02ab8db5d3fb
karysto <thanasi.karachotzitis@mail.utoronto.ca>
2015-03-16 15:40:45 -0400

    Integrate commander.js

--- minify.js
+++ minify.js
@@ -1,8 +1,31 @@
 #!/usr/bin/env node
 
 const
-  fs        = require('fs')
-  commander = require('commander');
+  fs      = require('fs')
+  program = require('commander');
+
+program
+  .version('0.0.1')
+  .description('A command line script to minify files.')
+  .option('-f, --file <file>', 'Specify the file to minify: minify -f thisFile.js')
+  .parse(process.argv);
+
+if (program.file) {
+  /* Read the file into a string */
+  var filePath       = program.file
+    , fileContent    = fs.readFileSync(filePath, 'utf8')
+    , minFileContent = minifyString(fileContent)
+    , minFileName    = addMinExtension(filePath);
+
+  var minifiedFile = fs.writeFile(minFileName, minFileContent, function(err) {
+    if (err) { throw err; };
+    console.log('-- ' + filePath + ' minified to ' + minFileName + ' --');
+  });
+};
+
+/***************************************/
+//         Helper Functions            //
+/***************************************/
 
 /* Remove all white space from str. */
 function minifyString(str) {
@@ -18,18 +41,3 @@ function addMinExtension(FileName) {
 
   return minFileName;
 };
-
-/* Removes white space from file and writes minified data to a new file. */
-(function main() {
-
-  /* Read the file into a string */
-  var filePath       = process.argv[2]
-    , fileContent    = fs.readFileSync(filePath, 'utf8')
-    , minFileContent = minifyString(fileContent)
-    , minFileName    = addMinExtension(filePath);
-
-  var minifiedFile = fs.writeFile(minFileName, minFileContent, function(err) {
-    if (err) { throw err; };
-    console.log('-- ' + filePath + ' minified to ' + minFileName + ' --');
-  });
-})();
